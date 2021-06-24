feccad1d2a2b6a2afdf348eb63b989ce31b13078
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-06-25 01:42:26 +0300

    further front end updates

--- main/views.py
+++ main/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from .models import Question
 
 
+def registerPage(request):
+    context = {
+    }
+    return render(request, "register.html", context)
+
+
 def homePage(request):
     questions = Question.objects.all().order_by('-created_at')
     context = {
         'questions': questions
     }
     return render(request, 'homepage.html', context)
+
 
 def questionPage(request, id):
     return None
