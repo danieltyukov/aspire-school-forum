78041f561b2a07b65220faf45b01a22302dd933e
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-06-25 19:15:19 +0300

    updates to the front and url management

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register', views.registerPage, name='register'),
     path('', views.homePage, name='index'),
-    path('question/<int:id>',views.questionPage, name='question')
+    path('question/<int:id>', views.questionPage, name='question')
 ]
