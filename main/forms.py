159d63060b6c7ded33ea50b1aa8be1228c83cfa6
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-06-27 16:35:29 +0300

    set up the css file and updated the register page

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,20 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from .models import Question, Response
+from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'required': True,
+                'placeholder': 'username@example.com',
+                'autofocus': True
+            }),
+            'username': forms.TextInput(attrs={
+                'placeholder': 'danieltyukov',
+            })
+        }
